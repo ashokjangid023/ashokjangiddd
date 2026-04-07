418cf9b8bbe139817c225605aabd6374a1641f74
Ashok Jangid <ashokjangid023@gmail.com>
2026-04-07 11:22:05 +0530

    fix: splitText issue and build fix

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -12,15 +12,21 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
+
   if (window.innerWidth < 900) return;
+
   const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
   const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
 
-  const TriggerStart = window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
+  const TriggerStart =
+    window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
+
   const ToggleAction = "play pause resume reverse";
 
+  // ✅ PARAGRAPHS
   paras.forEach((para: ParaElement) => {
     para.classList.add("visible");
+
     if (para.anim) {
       para.anim.progress(1).kill();
       para.split?.revert();
@@ -29,49 +35,53 @@ export default function setSplitText() {
     para.split = new SplitText(para, {
       type: "lines,words",
       linesClass: "split-line",
-    });
+    }) as SplitText;
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      para.split?.words || [],
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
+        y: 0,
+        duration: 1,
+        ease: "power3.out",
+        stagger: 0.02,
         scrollTrigger: {
           trigger: para.parentElement?.parentElement,
           toggleActions: ToggleAction,
           start: TriggerStart,
         },
-        duration: 1,
-        ease: "power3.out",
-        y: 0,
-        stagger: 0.02,
       }
     );
   });
+
+  // ✅ TITLES
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
-    });
+    }) as SplitText;
+
     title.anim = gsap.fromTo(
-      title.split.chars,
+      title.split?.chars || [],
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
+        y: 0,
+        rotate: 0,
+        duration: 0.8,
+        ease: "power2.inOut",
+        stagger: 0.03,
         scrollTrigger: {
           trigger: title.parentElement?.parentElement,
           toggleActions: ToggleAction,
           start: TriggerStart,
         },
-        duration: 0.8,
-        ease: "power2.inOut",
-        y: 0,
-        rotate: 0,
-        stagger: 0.03,
       }
     );
   });
